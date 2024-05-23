26f151deff0350c8633de0bac56e792506fd35d4
Denduff <Andreas.elbyhoej@live.dk>
2024-05-23 21:27:56 +0200

    finished

--- app/gameLogic.js
+++ app/gameLogic.js
@@ -115,7 +115,7 @@ export function getNextPlayer() {
     let playerArr = [...players];
     let index = playerArr.findIndex(p => p.name === gameStatus.currentPlayer.name);
     //turn 16 denotes the end of the game as all players have picked their 15 fields.
-    //Add some handling that says game is over.
+
     if(index === playerArr.length-1 && gameStatus.turn < 16) {
         gameStatus.turn++;
         index = -1
--- test/getNextPlayer.test.js
+++ test/getNextPlayer.test.js
@@ -13,24 +13,10 @@ describe("For getNextPlayer()", () => {
 
         setPlayers(playersss)
         setCurrentPlayer(jan)
-        getNextPlayer()
+        let cPlayer = getNextPlayer();
+        setCurrentPlayer(cPlayer);
 
         assert.sameMembers(players, playersss, "players succesfully added to array players")
-        assert.equal(getCurrentPlayer(), kaj, "set currentPlayer correctly to next player")
+        assert.equal(gameStatus.currentPlayer, kaj, "set currentPlayer correctly to next player")
     })
 })
-
-/*
-export function getNextPlayer() {
-    let playerArr = [...players];
-    let index = playerArr.findIndex(p => p.name === gameStatus.currentPlayer.name);
-    //turn 16 denotes the end of the game as all players have picked their 15 fields.
-    //Add some handling that says game is over.
-    if(index === playerArr.length-1 && gameStatus.turn < 16) {
-        gameStatus.turn++;
-        index = -1
-    }
-
-   return playerArr[index +1]
-}
-*/
